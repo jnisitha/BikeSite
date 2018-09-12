28f4d73703f55db6c6d333c1de37ab9fd1b8396f
vonru <enjay0714@gmail.com>
2018-09-12 16:08:03 -0400

    Completed form entry with database creation

--- app.js
+++ app.js
@@ -1,36 +1,74 @@
 var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/bike_routes");//intially creates the DB and subsequently connects to it.
 app.use(bodyParser.urlencoded({extended: true}));
 app.use( express.static( "public" ) );
 
 app.set("view engine", "ejs");
 
+//Schema Setup
+var bikeRoutesSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    author: String
+});
+
+var BikeRouteMod = mongoose.model("BikeRoute", bikeRoutesSchema);//creates the model from the schema
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-var bikeroutes = [
-    {title: "Niagara Route", image: "/images/biketrip.jpg", author: "Nisitha"},
-    {title: "Winter Biking", image: "/images/Branch.jpg", author: "Makroo"},
-    {title: "Harbour Front Trail", image: "/images/windmill.jpg", author: "Nisitha"},
-    {title: "Trip to Yelow Knife", image: "/images/YellowKnife.jpg", author: "Nisitha"}
-];
+// BikeRouteMod.create(
+//     {
+//         title: "Niagara Route", 
+//         image: "/images/biketrip.jpg", 
+//         author: "Nisitha"    
+//     }, function(err, bikeroutes){
+//         if(err){
+//             console.log(err);
+//         }else{
+//            console.log("created");
+//         }
+//     });
+// var bikeroutes = [
+//     {title: "Niagara Route", image: "/images/biketrip.jpg", author: "Nisitha"},
+//     {title: "Winter Biking", image: "/images/Branch.jpg", author: "Makroo"},
+//     {title: "Harbour Front Trail", image: "/images/windmill.jpg", author: "Nisitha"},
+//     {title: "Trip to Yelow Knife", image: "/images/YellowKnife.jpg", author: "Nisitha"}
+// ];
 
 
-app.get("/bikeroutes", function(req, res){    
-    res.render ("bikeroutes", {bikeroutes: bikeroutes});
+app.get("/bikeroutes", function(req, res){  
+    //Get BikeRoutes from the Database
+    BikeRouteMod.find({}, function(err, allBikeroutes){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("bikeroutes", {bikeroutes: allBikeroutes});
+        }
+    });
+    //res.render ("bikeroutes", {bikeroutes: bikeroutes});
 });
 
 app.post("/bikeroutes", function(req, res){
-    var name = req.body.title;
+    var title = req.body.title;
     var image = req.body.image;
     var author=req.body.author;
     var newBikeRoute = {title: title, image: image, author: author}
-    bikroutes.push(newBikeRoute);
+    //Create new Route and add to the DB
+    BikeRouteMod.create(newBikeRoute, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else {
+            res.redirect("/bikeroutes");
+        }
+    });
+    //bikroutes.push(newBikeRoute);
 
-    res.redirect("/bikeroutes");
 });
 
 app.get("/bikeroutes/new", function(req, res){
