fe3e01cbfba0dd4c55b4845f0dafe9004c05748f
vonru <enjay0714@gmail.com>
2018-09-11 23:54:51 -0400

    added new bikeroute functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use( express.static( "public" ) );
 
 app.set("view engine", "ejs");
@@ -9,15 +11,30 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/bikeroutes", function(req, res){
-    var bikeroutes = [
-        {title: "Niagara Route", image: "/images/biketrip.jpg", author: "Nisitha"},
-        {title: "Winter Biking", image: "/images/Branch.jpg", author: "Makroo"},
-        {title: "Harbour Front Trail", image: "/images/windmill.jpg", author: "Nisitha"},
-        {title: "Trip to Yelow Knife", image: "/images/YellowKnife.jpg", author: "Nisitha"}
-    ];
-    
+var bikeroutes = [
+    {title: "Niagara Route", image: "/images/biketrip.jpg", author: "Nisitha"},
+    {title: "Winter Biking", image: "/images/Branch.jpg", author: "Makroo"},
+    {title: "Harbour Front Trail", image: "/images/windmill.jpg", author: "Nisitha"},
+    {title: "Trip to Yelow Knife", image: "/images/YellowKnife.jpg", author: "Nisitha"}
+];
+
+
+app.get("/bikeroutes", function(req, res){    
     res.render ("bikeroutes", {bikeroutes: bikeroutes});
+});
+
+app.post("/bikeroutes", function(req, res){
+    var name = req.body.title;
+    var image = req.body.image;
+    var author=req.body.author;
+    var newBikeRoute = {title: title, image: image, author: author}
+    bikroutes.push(newBikeRoute);
+
+    res.redirect("/bikeroutes");
+});
+
+app.get("/bikeroutes/new", function(req, res){
+    res.render("new");
 });
 
 app.listen(3000, function(){
