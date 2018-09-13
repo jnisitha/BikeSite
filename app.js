c0707f8c6e7615c1046a768fddd472f77bdea03c
vonru <enjay0714@gmail.com>
2018-09-13 16:10:45 -0400

    added basic comment functionality

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-var express = require("express");
+var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     BikeRouteMod = require("./models/bikeroute"),
+    CommentMod = require("./models/comment"),
     seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/bike_routes");//intially creates the DB and subsequently connects to it.
-app.use(bodyParser.urlencoded({extended: true}));
-app.use( express.static( "public" ) );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("landing");
 });
 
 seedDB();
 
 //INDEX - 
-app.get("/bikeroutes", function(req, res){  
+app.get("/bikeroutes", function (req, res) {
     //Get BikeRoutes from the Database
-    BikeRouteMod.find({}, function(err, allBikeroutes){
-        if(err){
+    BikeRouteMod.find({}, function (err, allBikeroutes) {
+        if (err) {
             console.log(err);
-        }else{
-            res.render("bikeroutes/index", {bikeroutes: allBikeroutes});
+        } else {
+            res.render("bikeroutes/index", { bikeroutes: allBikeroutes });
         }
     });
     //res.render ("bikeroutes", {bikeroutes: bikeroutes});
 });
 
 //CREATE - add new to DB
-app.post("/bikeroutes", function(req, res){
+app.post("/bikeroutes", function (req, res) {
     var title = req.body.title;
     var image = req.body.image;
-    var author=req.body.author;
+    var author = req.body.author;
     var description = req.body.description;
-    var newBikeRoute = {title: title, image: image,description:description, author: author}
+    var newBikeRoute = { title: title, image: image, description: description, author: author }
     //Create new Route and add to the DB
-    BikeRouteMod.create(newBikeRoute, function(err, newlyCreated){
-        if(err){
+    BikeRouteMod.create(newBikeRoute, function (err, newlyCreated) {
+        if (err) {
             console.log(err);
-        }else {
+        } else {
             res.redirect("/bikeroutes");
         }
     });
@@ -50,17 +51,17 @@ app.post("/bikeroutes", function(req, res){
 });
 
 //NEW - show form to create new
-app.get("/bikeroutes/new", function(req, res){
+app.get("/bikeroutes/new", function (req, res) {
     res.render("bikeroutes/new");
 });
 
 //SHOW -  show information about one bike route
-app.get("/bikeroutes/:id", function(req, res){
-    BikeRouteMod.findById(req.params.id).populate("comments").exec(function(err, foundBikeRoute){
-        if(err){
+app.get("/bikeroutes/:id", function (req, res) {
+    BikeRouteMod.findById(req.params.id).populate("comments").exec(function (err, foundBikeRoute) {
+        if (err) {
             console.log(err);
-        }else{
-            res.render("bikeroutes/show", {bikeroute: foundBikeRoute});
+        } else {
+            res.render("bikeroutes/show", { bikeroute: foundBikeRoute });
         }
     });
 });
@@ -69,10 +70,38 @@ app.get("/bikeroutes/:id", function(req, res){
 //COMMENTS ROUTES
 //=====================
 
-app.get("/bikeroutes/:id/comments/new", function(req, res){
-    res.render("comments/new");
+//NEW
+app.get("/bikeroutes/:id/comments/new", function (req, res) {
+    //find bikeroute by id
+    BikeRouteMod.findById(req.params.id, function (err, foundBikeroute) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { bikeroute: foundBikeroute });
+        }
+    });    
 });
 
-app.listen(3000, function(){
+//POST
+app.post("/bikeroutes/:id/comments", function (req, res) {
+    BikeRouteMod.findById(req.params.id, function (err, bikeroute) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            CommentMod.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    bikeroute.comments.push(comment);
+                    bikeroute.save();
+                    res.redirect("/bikeroutes/" + bikeroute._id);
+                }
+            })
+        }
+    });
+});
+
+app.listen(3000, function () {
     console.log("BikeSite has started!");
 });
