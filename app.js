66ff6b39abc7d6c4932a9b33c2877eac4e323b36
vonru <enjay0714@gmail.com>
2018-09-15 16:44:14 -0400

    Added user authentication

--- app.js
+++ app.js
@@ -2,21 +2,46 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     BikeRouteMod = require("./models/bikeroute"),
     CommentMod = require("./models/comment"),
+    UserMod = require("./models/user"),
     seedDB = require("./seeds");
 
-mongoose.connect("mongodb://localhost/bike_routes");//intially creates the DB and subsequently connects to it.
+mongoose.connect("mongodb://localhost/bike_routes", {useNewUrlParser: true});//intially creates the DB and subsequently connects to it.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-    res.render("landing");
-});
 
 seedDB();
+
+//PASSPORT CONFIGURATION    
+app.use(require("express-session")({
+    secret: "Ish Shadei Dreiru",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(UserMod.authenticate()));
+passport.serializeUser(UserMod.serializeUser());
+passport.deserializeUser(UserMod.deserializeUser());
+
+//Middleware for passing user.
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.render("landing");    
+});
+
 
 //INDEX - 
 app.get("/bikeroutes", function (req, res) {
@@ -71,7 +96,7 @@ app.get("/bikeroutes/:id", function (req, res) {
 //=====================
 
 //NEW
-app.get("/bikeroutes/:id/comments/new", function (req, res) {
+app.get("/bikeroutes/:id/comments/new", isLoggedIn, function (req, res) {
     //find bikeroute by id
     BikeRouteMod.findById(req.params.id, function (err, foundBikeroute) {
         if (err) {
@@ -79,11 +104,11 @@ app.get("/bikeroutes/:id/comments/new", function (req, res) {
         } else {
             res.render("comments/new", { bikeroute: foundBikeroute });
         }
-    });    
+    });
 });
 
 //POST
-app.post("/bikeroutes/:id/comments", function (req, res) {
+app.post("/bikeroutes/:id/comments", isLoggedIn, function (req, res) {
     BikeRouteMod.findById(req.params.id, function (err, bikeroute) {
         if (err) {
             console.log(err);
@@ -101,6 +126,57 @@ app.post("/bikeroutes/:id/comments", function (req, res) {
         }
     });
 });
+
+//==============
+//AUTH ROUTES
+//==============
+
+//show registering form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+//Signing up.
+app.post("/register", function (req, res) {
+    var newUser = new UserMod({ username: req.body.username });
+    UserMod.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/bikeroutes");
+        });
+    });
+});
+
+//Show Login Form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+//Logging in (POST)
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/bikeroutes",
+        failureRedirect: "/login"
+    }), function (req, res) {
+
+});
+
+//Logout route
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/bikeroutes");
+});
+
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function () {
     console.log("BikeSite has started!");
